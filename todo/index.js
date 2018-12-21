937939f51d32aa02f16f5aebd896cd76e9c48754
M Haidar Hanif <git@mhaidarhanif.com>
2018-12-21 16:14:57 +0700

    Implement complete or incomplete a todo

--- todo/index.js
+++ todo/index.js
@@ -2,6 +2,8 @@ const formInput = document.getElementById("input")
 const output = document.getElementById("output")
 const buttonClear = document.getElementById("button-clear")
 
+// -----------------------------------------------------------------------------
+
 // Handle form on submit, when user entering a new todo
 const submit = event => {
   // Prevent default behavior of form (reload the page)
@@ -24,13 +26,15 @@ const submit = event => {
     // Save the new data into localStorage
     save(DATA)
 
-    // Display again the
+    // Display todos again
     display()
 
     // Empty out the input todo text value
     inputTodo.value = ""
   }
 }
+
+// -----------------------------------------------------------------------------
 
 // Display todos in the output area
 const display = () => {
@@ -42,19 +46,41 @@ const display = () => {
 
   // Loop over the array of objects
   // Put them into <ul> output as multiple <div>
-  DATA.forEach(item => {
-    output.innerHTML += `<div>
+  DATA.forEach((item, index) => {
+    output.innerHTML += `<div onclick="complete(${index})">
       <div class="box ${item.completed ? "completed" : ""}"></div>
-      <span class="todo-text">
+      <span class="todo">
         ${item.text}
       </span>
     </div>`
   })
 }
 
+// -----------------------------------------------------------------------------
+
+// Complete or incomplete a todo
+const complete = indexToCheck => {
+  // Load latest data from localStorage
+  const DATA = load()
+
+  // If the todo index is matched with the clicked one,
+  // the completed value will be inverted (true/false)
+  const DATA_NEW = DATA.map((item, index) => {
+    if (index === indexToCheck) item.completed = !item.completed
+    return item
+  })
+
+  save(DATA_NEW)
+  display()
+}
+
+// -----------------------------------------------------------------------------
+
 // Listen for submit event in the form
 formInput.addEventListener("submit", submit)
 buttonClear.addEventListener("click", clear)
+
+// -----------------------------------------------------------------------------
 
 // Display all todos once in the beginning
 display()
