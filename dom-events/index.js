c032459b28543d269f2f1b2246a072d14f0c562f
M Haidar Hanif <git@mhaidarhanif.com>
2018-12-21 11:30:01 +0700

    Create example for DOM events with form

--- dom-events/index.js
+++ dom-events/index.js
@@ -1,1 +1,37 @@
 console.log("DOM Events")
+
+const formGuestList = document.getElementById("guest-list")
+const preOutput = document.getElementById("output-code")
+const ulOutput = document.getElementById("output-ul")
+
+const submitGuestData = event => {
+  event.preventDefault()
+
+  const inputGuestFullName = document.getElementById("guest-full-name").value
+  const inputGuestPhone = document.getElementById("guest-phone").value
+  const inputGuestEmail = document.getElementById("guest-email").value
+
+  const data = {
+    name: inputGuestFullName,
+    phone: inputGuestPhone,
+    email: inputGuestEmail
+  }
+
+  displayToList(data)
+}
+
+const displayToConsole = data => {
+  console.log(data)
+}
+
+const displayToPre = data => {
+  const text = JSON.stringify(data)
+  preOutput.innerText = text
+}
+
+const displayToList = data => {
+  const guest = `${data.name} (${data.phone}) <${data.email}>`
+  ulOutput.innerHTML += `<li>${guest}</li>`
+}
+
+formGuestList.addEventListener("submit", submitGuestData)
