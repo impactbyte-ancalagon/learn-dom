03e9dfb831573d9f8a8402297898c4972ec30e05
M Haidar Hanif <git@mhaidarhanif.com>
2018-12-21 10:35:07 +0700

    Create basic DOM traversal and manipulation

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Learn DOM")
+
+const h2 = document.createElement("h2")
+const heading2 = document.createTextNode("Learn DOM with JavaScript")
+
+h2.appendChild(heading2)
+
+const content = document.getElementById("content")
+content.appendChild(h2)
